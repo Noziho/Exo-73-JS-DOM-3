ad0f6579d617dcf450409f2fc24877759bc1f0b2
Noah2211 <noah.decroix3@gmail.com>
2021-11-18 09:44:20 +0100

    exercice 2 done

--- exo 2/exercice.js
+++ exo 2/exercice.js
@@ -1,6 +1,13 @@
 let colorEl = document.getElementById("trendy-color");
+colorEl.innerText = "Blue";
+colorEl.style.color = "blue";
+
 let handEl  = document.getElementById("trendy-handwriting");
+handEl.innerText = "Nekfeu";
+handEl.style.fontFamily = "arial"
+
 let frameEl = document.getElementById("trendy-frame");
+frameEl.style.border = "dashed 2px black";
 
 
 
