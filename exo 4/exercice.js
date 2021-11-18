2839f82fbf946a00d369746b21e4b03838ca94f7
Noah2211 <noah.decroix3@gmail.com>
2021-11-18 10:02:19 +0100

    exercice done, j'ai pas compris l'exo 3

--- exo 4/exercice.js
+++ exo 4/exercice.js
@@ -1,5 +1,21 @@
+let planet = document.createElement('div');
+let lune = document.createElement('div');
 
 
+
+planet.style.background = "green";
+planet.className = "Planet";
+planet.style.width = "400px";
+planet.style.height = "400px";
+planet.style.borderRadius = "200px";
+document.body.append(planet);
+
+lune.style.background = "white";
+lune.className = "lune";
+lune.style.width = "200px";
+lune.style.height = "200px";
+lune.style.borderRadius = "200px";
+document.body.append(lune);
 
 
 
