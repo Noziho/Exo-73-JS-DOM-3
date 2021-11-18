ba0b9f0e7f149d2e299eb363fbb49cb98cfe583c
Noah2211 <noah.decroix3@gmail.com>
2021-11-18 09:39:56 +0100

    exercice 1 done

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,5 +1,13 @@
 let avatarEls = document.getElementsByTagName("img");
 
+for (i = 0; i < avatarEls.length; i++) {
+
+    avatarEls[i].style.height = "400px";
+    avatarEls[0].title = "Marcimus, A pink, innocent avatar";
+    avatarEls[1].title = "Old Spice Mann, A green, angry avatar";
+    avatarEls[2].title = "Orange Juice Squid, An orange, multi-legged avatar";
+}
+
 
 
 
